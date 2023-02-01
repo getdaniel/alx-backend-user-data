2327898fca66e9a9287e842b715ac9f1e0019887
Me <getdaniel>
2023-02-01 10:40:59 +0300

    log formater

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Personal Data."""
+import logging
 import re
 from typing import List
 
@@ -18,3 +19,24 @@ def filter_datum(
     """ Returns the log message obfuscated."""
     extract, replace = (patterns['extract'], patterns['replace'])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialize the RedactingFormatter class."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log Record."""
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+        return txt
