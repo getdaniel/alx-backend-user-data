e71cd303cc7df3ea90308979555b1f9371c2eedd
Me <getdaniel>
2023-02-02 00:03:36 +0300

    fix the importing style

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Encrypting Password."""
-from bcrypt import checkpw, gensalt, hashpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string."""
-    return hashpw(password.encode('utf-8'), gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Checks valid password."""
-    return checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
