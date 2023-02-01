34248acecfbbe2640d556ddf2dd8881b18fd4037
Me <getdaniel>
2023-02-01 13:51:44 +0300

    check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """ Encrypting Password."""
-from bcrypt import gensalt, hashpw
+from bcrypt import checkpw, gensalt, hashpw
 
 
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string."""
     return hashpw(password.encode('utf-8'), gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks valid password."""
+    return checkpw(password.encode('utf-8'), hashed_password)
