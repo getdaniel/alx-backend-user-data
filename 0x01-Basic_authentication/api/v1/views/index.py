69fbaded014a7ee3c8f5d279e9019d6ea13604b9
Me <getdaniel>
2023-02-06 13:45:00 +0300

    error handler: forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> None:
     """ GET /api/v1/unauthorized.
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden.
+    """
+    abort(403)
