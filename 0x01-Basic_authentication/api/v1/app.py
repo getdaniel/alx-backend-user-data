8cde3e3df022f4a3b36be57c30e88784c09691f0
Me <getdaniel>
2023-02-06 17:25:13 +0300

    request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,12 +7,21 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+#from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv("AUTH_TYPE", 'auth')
 
+if auth_type == "auth":
+    auth = Auth()
+
+#elif auth_type == "basic_auth":
+ #   auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +42,23 @@ def forbidden(error) -> str:
     """ forbidde error handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_handler():
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,24 +9,28 @@ class Auth:
     """ Manage the API authentication."""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Implements the API authentication mgt."""
-        if path is None:
-            return True
-
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-
-        for ex_path in excluded_paths:
-            if path == ex_path or path.startswith(ex_path[:-1]):
-                return False
-
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """ Implements autherization header."""
-        if request is not None:
-            return request.headers.get('Authorization', None)
+        if request is None:
+            return None
 
-        return None
+        if "Authorization" not in request.headers:
+            return None
+
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Implements the current user request status."""
