fda8508d8bb70177dc3daa1bb325cc3e5376f337
Me <getdaniel>
2023-02-06 23:59:09 +0300

    refactor to fix the error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def raise_unauthorized() -> None:
+    """ GET /api/v1/unauthorized
+    Raise:
+      - a 401 error
+    """
+    abort(401)
