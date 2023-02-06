c38445e5549b6f2b026863e9d5d88a95572b52ed
Me <getdaniel>
2023-02-06 17:30:47 +0300

    basic auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
-#from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -20,8 +20,8 @@ auth_type = getenv("AUTH_TYPE", 'auth')
 if auth_type == "auth":
     auth = Auth()
 
-#elif auth_type == "basic_auth":
- #   auth = BasicAuth()
+elif auth_type == "basic_auth":
+    auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
