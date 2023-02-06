d2324b66ba4ccd5dce99e70448379f0f5d34e8d9
Me <getdaniel>
2023-02-07 00:31:21 +0300

    update and refactor

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,14 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns False
         """
-        return False
+        if not path:
+            return True
+        if not excluded_paths or excluded_paths == []:
+            return True
+        for p in excluded_paths:
+            if path == p[:-1] or path.startswith(p):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns None
