db55a137e0719fe9dc407eda12058f06ac50eb76
Me <getdaniel>
2023-02-06 23:52:57 +0300

    refactor again

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,17 +24,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
-    """ GET /api/v1/unauthorized.
-    """
-    abort(401)
-
-
-@app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
-    """ GET /api/v1/forbidden.
-    """
-    abort(403)
