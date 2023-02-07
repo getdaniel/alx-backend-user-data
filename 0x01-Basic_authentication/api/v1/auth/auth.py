9711b3510f02be806b10a5cb71f9df903bb822db
Me <getdaniel>
2023-02-07 10:46:57 +0300

    refactore request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,11 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Returns None
         """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None
