01670f152c96f2f8f59fdbf6d5bd5024f1326fe2
Me <getdaniel>
2023-02-07 11:12:40 +0300

    basic - base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic authentication class."""
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -18,3 +20,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ")[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """ Decodes the base64 autherization header."""
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(
+                    base64_authorization_header).decode('utf-8')
+        except (UnicodeDecodeError, binascii.Error):
+            return None
+
+        return decoded
