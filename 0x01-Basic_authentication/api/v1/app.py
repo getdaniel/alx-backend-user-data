215dda180aecd46762514a336ec57731514c50a1
Me <getdaniel>
2023-02-07 10:59:12 +0300

    refactor basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -17,7 +18,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv("AUTH_TYPE", 'auth')
 
-if auth_type == "auth":
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+elif auth_type == "auth":
     auth = Auth()
 
 
