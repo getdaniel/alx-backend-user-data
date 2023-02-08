b63a80b9f1fd09370b8df8d4f6e9e8aed6815e55
Me <getdaniel>
2023-02-08 23:34:21 +0300

    Boooooom

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -8,7 +8,7 @@ from models.user_session import UserSession
 
 class SessionDBAuth(SessionExpAuth):
     """ Implements session based on DB."""
-    def create_session(self, user_id=None) -> str:
+    def create_session(self, user_id=None):
         """Creates and stores a session id for the user."""
         session_id = super().create_session(user_id)
         if type(session_id) == str:
@@ -32,8 +32,8 @@ class SessionDBAuth(SessionExpAuth):
             return None
         current_time = datetime.now()
         time_span = timedelta(seconds=self.session_duration)
-        exp_time = sessions[0].created_at + time_span
-        if exp_time < current_time:
+        expire_time = sessions[0].created_at + time_span
+        if expire_time < current_time:
             return None
         return sessions[0].user_id
 
