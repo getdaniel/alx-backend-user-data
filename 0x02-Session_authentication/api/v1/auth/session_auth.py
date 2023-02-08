7d100fca7c5597b0bf9f53c3560dead3dd227ded
Me <getdaniel>
2023-02-08 12:26:50 +0300

    session class with two methods

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Session Management Class."""
+from api.v1.auth.auth import Auth
+import uuid
+
+
+class SessionAuth(Auth):
+    """ Implements session authentication."""
+    user_id_by_session_id = {}
+
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a session."""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ User ID for Session ID."""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
