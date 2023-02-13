f5ed5980865e7e13cc14655a03d999fd69960d0f
Me <getdaniel>
2023-02-13 15:11:11 +0300

    credential validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,3 +39,14 @@ class Auth:
         except:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Try locating the user by email.
+        If it exists, check the password with bcrypt.checkpw.
+        If it matches return True. In any other case, return False.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except Exception:
+            return False 
