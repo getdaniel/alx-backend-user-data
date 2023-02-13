d19e11c63d38ebabc83c4bc5b6f628953c5d5b25
Me <getdaniel>
2023-02-13 16:01:03 +0300

    find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """ Authentication Implementation modules."""
 import bcrypt
 from db import DB
+from typing import Union
 from user import User
 import uuid
 
@@ -67,3 +68,14 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """ Finds user by session ID."""
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
