07de1afb6fa874b2652e2e7dd9a8611d8a2bcf99
Me <getdaniel>
2023-02-14 11:24:49 +0300

    ...

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,11 +3,10 @@
 """
 from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
 from user import Base, User
 
 
