1069be6ef9c57ff329a6a8221b2dabebf6da3e6f
Me <getdaniel>
2023-02-17 11:48:19 +0300

    change mail to email

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,7 +80,7 @@ def get_reset_password_token():
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     """Update password end-point"""
-    mail = request.form.get("email")
+    email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
 
