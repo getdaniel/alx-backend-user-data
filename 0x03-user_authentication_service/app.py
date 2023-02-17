479d07177fd275d58174f6a21fe293d87adbc3c0
Me <getdaniel>
2023-02-17 11:13:31 +0300

    refactor the update user end point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,24 +80,18 @@ def get_reset_password_token():
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     """Update password end-point"""
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-
+    mail = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    is_password_changed = False
     try:
-        user = AUTH.get_user_from_reset_token(reset_token)
+        AUTH.update_password(reset_token, new_password)
+        is_password_changed = True
     except ValueError:
+        is_password_changed = False
+    if not is_password_changed:
         abort(403)
-
-    if user.email != email:
-        abort(403)
-
-    AUTH.update_password(reset_token, new_password)
-
-    return jsonify({
-        "email": email,
-        "message": "Password updated"
-    }), 200
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
