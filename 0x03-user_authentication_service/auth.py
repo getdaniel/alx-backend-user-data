1d4dbc1e707779dd9e2db05a639e569b79647ce0
Me <getdaniel>
2023-02-17 11:05:00 +0300

    refactor the update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -107,11 +107,16 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """ Updates the user password."""
+        user = None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            raise ValueError
-
-        user.reset_token = None
-        user.hashed_password = _hash_password(password)
-        self._db.commit()
+            user = None
+        if user is None:
+            raise ValueError()
+        new_password_hash = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=new_password_hash,
+            reset_token=None,
+        )
